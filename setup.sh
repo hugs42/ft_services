e29b52c214f61968b960af56739b4eb7a1c439c1
hugs42 <hugsbord@student.42.fr>
2021-11-10 17:34:04 +0100

    nginx wordpress phpmyadmin and ftps config in progress

--- setup.sh
+++ setup.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 
-banner ()
-{
-	echo "\033[96m"
-	echo "                            :::      ::::::::"
-	echo "                          :+:      :+:    :+:"
-	echo "    42_cursus:          +:+ +:+         +:+"
-	echo "    FT_SERVICES       +#+  +:+       +#+"
-	echo "                    +#+#+#+#+#+   +#+"
-	echo "                          #+#    #+#"
-	echo "                         ###   ########.fr\033[0m"
-	echo "\033[0m"
-}
 
-minikube ()
-{
-	echo "Starting minikube's configuration"
-	minikube delete
-}
+echo "\033[96m"
+echo "                            :::      ::::::::"
+echo "                          :+:      :+:    :+:"
+echo "    42_cursus:          +:+ +:+         +:+"
+echo "    FT_SERVICES       +#+  +:+       +#+"
+echo "                    +#+#+#+#+#+   +#+"
+echo "                          #+#    #+#"
+echo "                         ###   ########.fr\033[0m"
+echo "\033[0m"
+sleep(5)
 
-main ()
-{
-	banner
-
-}
-
-main
+echo "Starting minikube's configuration"
+minikube delete
+killall -TERM kubectl minikube VboxHeadless
+minikube start --driver=docker --cpus=2
+eval $(minikube docker-env)
+minikube addons enable metrics-server > /dev/null
+minikube addons enable dashboard &> /dev/null
+minikube addons enable metallb
+docker build -t nginx ./srcs/nginx/ > /dev/null
+docker build -t phpmyadmin ./srcs/phpmyadmin/ > /dev/null
+docker build -t wordpress ./srcs/wordpress/ > /dev/null
+docker build -t ftps ./srcs/ftps/ > /dev/null
