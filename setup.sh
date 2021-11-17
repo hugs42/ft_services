25569b9a5ba4d2ff1172d657412628e576f82dfc
hugs42 <hugsbord@student.42.fr>
2021-11-17 15:51:46 +0100

    nginx mysql phpmyadmin build without errors

--- setup.sh
+++ setup.sh
@@ -14,24 +14,26 @@ echo "Starting minikube's configuration"
 minikube stop
 minikube delete
 minikube start --driver=docker --cpus=2
+eval $(minikube docker-env)
 
 minikube addons enable metrics-server
 minikube addons enable dashboard
 minikube addons enable metallb
 
-#eval $(minikube docker-env)
+sleep 5
 
 docker build -t nginx ./srcs/nginx/
-docker build -t phpmyadmin ./srcs/phpmyadmin/
+docker build -t phpmyadmin ./srcs/phpmyadmin/ > /dev/null
 docker build -t mysql ./srcs/mysql/
-docker build -t wordpress ./srcs/wordpress/ > /dev/null
-docker build -t ftps ./srcs/ftps/
+#docker build -t wordpress ./srcs/wordpress/ > /dev/null
+#docker build -t ftps ./srcs/ftps/
+#docker build -t grafana ./srcs/ftps/
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
-kubectl apply -f ./srcs/yalm/nginx.yalm
-kubectl apply -f ./srcs/yalm/ftps.yalm
-kubectl apply -f ./srcs/yalm/phpmyadmin.yalm
-kubectl apply -f ./srcs/yalm/wordpress.yalm
-
+#kubectl apply -f ./srcs/yalm/nginx.yalm
+#kubectl apply -f ./srcs/yalm/ftps.yalm
+#kubectl apply -f ./srcs/yalm/phpmyadmin.yalm
+#kubectl apply -f ./srcs/yalm/wordpress.yalm
+#kubectl apply -f ./srcs/yalm/grafana.yalm
 #minikube dashboard
