472985cced2985a0ca188f283770687805cfd434
hugs42 <hugsbord@student.42.fr>
2021-11-17 16:08:25 +0100

    nginx and metallb yalm work

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ minikube addons enable metallb
 sleep 5
 
 docker build -t nginx ./srcs/nginx/
-docker build -t phpmyadmin ./srcs/phpmyadmin/ > /dev/null
+docker build -t phpmyadmin ./srcs/phpmyadmin/
 docker build -t mysql ./srcs/mysql/
 #docker build -t wordpress ./srcs/wordpress/ > /dev/null
 #docker build -t ftps ./srcs/ftps/
@@ -31,7 +31,9 @@ docker build -t mysql ./srcs/mysql/
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
-#kubectl apply -f ./srcs/yalm/nginx.yalm
+
+kubectl apply -f ./srcs/yalm/metallb_configmap.yalm
+kubectl apply -f ./srcs/yalm/nginx.yalm
 #kubectl apply -f ./srcs/yalm/ftps.yalm
 #kubectl apply -f ./srcs/yalm/phpmyadmin.yalm
 #kubectl apply -f ./srcs/yalm/wordpress.yalm
