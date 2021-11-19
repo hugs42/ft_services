2f6662421abdb3f87b0f5f78deac0f77752f7328
hugs42 <hugsbord@student.42.fr>
2021-11-19 11:45:53 +0100

    errors in configmap IP fixed but pods still CrashLoopBackOff

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ echo "Starting minikube's configuration"
 minikube stop
 minikube delete
 minikube start --driver=docker --cpus=2
+#--kubernetes-version v1.24.0
 eval $(minikube docker-env)
 
 minikube addons enable metrics-server
@@ -26,20 +27,21 @@ docker build -t nginx ./srcs/nginx/ #> /dev/null
 docker build -t mysql ./srcs/mysql/ #> /dev/null
 docker build -t phpmyadmin ./srcs/phpmyadmin/ #> /dev/null
 docker build -t wordpress ./srcs/wordpress/ #> /dev/null
-#docker build -t ftps ./srcs/ftps/
-#docker build -t grafana ./srcs/ftps/
+docker build -t ftps ./srcs/ftps/
+docker build -t grafana ./srcs/grafana/
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.11.0/manifests/metallb.yaml
+
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 kubectl apply -f ./srcs/yalm/metallb_configmap.yalm
 kubectl apply -f ./srcs/yalm/nginx.yalm
 kubectl apply -f ./srcs/yalm/mysql.yalm
 kubectl apply -f ./srcs/yalm/phpmyadmin.yalm
 kubectl apply -f ./srcs/yalm/wordpress.yalm
-
-#kubectl apply -f ./srcs/yalm/ftps.yalm
-#kubectl apply -f ./srcs/yalm/grafana.yalm
-#kubectl apply -f ./srcs/yalm/influxdb.yalm
+kubectl apply -f ./srcs/yalm/ftps.yalm
+kubectl apply -f ./srcs/yalm/grafana.yalm
+kubectl apply -f ./srcs/yalm/influxdb.yalm
 
 #minikube dashboard
