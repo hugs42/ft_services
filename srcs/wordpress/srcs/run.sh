5943d25178a2d407a61d5d548a972ca2ec23ba71
hugs42 <hugsbord@student.42.fr>
2021-12-04 13:00:30 +0100

    all pods works together

--- srcs/wordpress/srcs/run.sh
+++ srcs/wordpress/srcs/run.sh
@@ -1,8 +1,3 @@
-mkdir -p /run/nginx
-mkdir -p /etc/telegraf
-telegraf -sample-config --input-filter cpu:mem:net:swap:diskio --output-filter influxdb > /etc/telegraf/telegraf.conf
-sed -i s/'# urls = \["http:\/\/127.0.0.1:8086"\]'/'urls = ["http:\/\/influxdb:8086"]'/ /etc/telegraf/telegraf.conf
-sed -i s/'# database = "telegraf"'/'database = "wordpress"'/ /etc/telegraf/telegraf.conf
-sed -i s/'omit_hostname = false'/'omit_hostname = true'/ /etc/telegraf/telegraf.conf
+telegraf -config /etc/telegraf.conf &
 php-fpm7 &
 nginx -g 'daemon off;'
